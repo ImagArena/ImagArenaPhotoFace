973e732959e2f54dc057a6ae6c92ff69e221cf2e
Ubuntu <ubuntu@ip-172-31-1-114.us-west-2.compute.internal>
2017-04-10 22:23:29 +0000

    port 80

--- client/index.js
+++ client/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 
 const host = '0.0.0.0';
-const port = '8000';
+const port = '80';
 
 const app = express();
-app.use(express.static(__dirname))
+app.use(express.static(__dirname + "/dist/"))
 
 app.listen(port, host, (err) => {
 	if(err) console.log(err);
 	console.log(`listening on port ${port}`);
+//	app.get('*', (req, res) => res.sendFile(__dirname + '/dist/index.html'));
 });
