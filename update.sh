5ff508f645214f356046a08cd9fcf3b63872ba47
Elliot Berman <elliotjberman@gmail.com>
2017-04-27 22:13:33 +0200

    ssh?

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ git add .
 git commit -m $commit_message
 git push
 
-ssh -i /Users/TimMerle/.ssh/id_rsa bleachercreature@34.223.254.57
+ssh -i /Users/TimMerle/Desktop/Tim_Key.pem ubuntu@34.223.254.57
 cd ImagArenaPhotoFace
 git pull
 cd client
