849792b0f5e15ceb7a30b2bfc5e646d27e9b0980
Elliot Berman <elliotjberman@gmail.com>
2017-05-11 19:57:28 +0200

    google analytics

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-74261860-1');
+
 import Main from './components/Main';
 import Home from './components/Home';
 import Programs from './components/Programs';
@@ -14,8 +17,13 @@ import Materials from './components/Materials';
 import Choose from './components/Choose';
 import Photos from './components/Photos';
 
+browserHistory.listen(function (location) {
+		window.ga('send', 'pageview', location.pathname);
+});
+
 // Render the main component into the dom
 ReactDOM.render((
+
 <Router history={browserHistory}>
 
 	<Route path='/' component={Main}>
