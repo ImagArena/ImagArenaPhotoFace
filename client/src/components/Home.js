b49b8f2963e3a75fbc295c38654bac97b3f2f850
Elliot Berman <elliotjberman@gmail.com>
2017-07-09 23:12:09 -0400

    mobile fixes

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -13,12 +13,15 @@ class Home extends React.Component {
   render = () => {
     return (
 			<div className="index">
-				<div id="homegif" className="col-md-8 col-md-offset-2">
-					<div id="home-video" className="videoWrapper">
-						<iframe width="560" height="315" src="https://www.youtube.com/embed/9yvGqvQjs7s" frameborder="0" allowfullscreen></iframe>
-					</div>
+				<div className="container-fluid">
+					<div className="col-md-8 col-md-offset-2">
+						<div id="home-video" className="videoWrapper">
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/9yvGqvQjs7s" frameborder="0" allowfullscreen></iframe>
+						</div>
 
+					</div>
 				</div>
+
 	      <div >
 					<h2 className="main-header" id="hometext">ImagArena® is a <strong>building workshop</strong> for <strong>ages 6+</strong>, designed to awaken the imaginative abilities of <strong>every individual</strong>.</h2>
 				</div>
