1b48adb5514fc80592c701263745e065dad91e66
sh0shinsha <merletim@gmail.com>
2017-10-21 17:56:05 -0400

    learning
    uppercase

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -70,7 +70,7 @@ class Home extends React.Component {
         {/* banners */}
         <div className="">
           <div className="videoholder">
-            <h1>Expand Your Imagination</h1>
+            <h1>Expand your imagination</h1>
             <a id="video-play" data-toggle="modal" data-target="#video-modal">
               <img src={require("../images/play-button.svg")} />
             </a>
